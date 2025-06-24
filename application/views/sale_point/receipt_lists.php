93e9596e73f94f333508fc2a14df67be04d5cdc1
Navid Aziz <navidaziz98@gmail.com>
2025-06-24 15:23:22 +0500

    some changes

--- application/views/sale_point/receipt_lists.php
+++ application/views/sale_point/receipt_lists.php
@@ -3,13 +3,6 @@
         <th>#</th>
         <th>Sale ID</th>
         <th>Date</th>
-        <th></th>
-        <th>Payment Type</th>
-        <th>Customer Name</th>
-        <!-- <th>Mobile Number</th> -->
-        <th>Items Price (Rs)</th>
-        <th>Tax</th>
-        <th>Discount (Rs)</th>
         <th>Total Amount (Rs)</th>
         <th>Receipts</th>
     </tr>
@@ -22,16 +15,6 @@
             <td>
                 <small><?php echo date("dM,y", strtotime($sale->created_date)) ?></small>
             </td>
-            <td>
-
-                <?php if ($sale->return) { ?> <span style="color: red;">Return</span> <?php } else { ?> <span style="color: green;">Sale</span> <?php } ?>
-            </td>
-            <td><?php echo $sale->payment_type ?></td>
-            <td><?php echo $sale->customer_name; ?></td>
-            <!-- <td><?php echo $sale->customer_mobile_no; ?></td> -->
-            <td><?php echo $sale->items_total_price; ?></td>
-            <td><?php echo $sale->total_tax_pay_able; ?></td>
-            <td><?php echo $sale->discount; ?></td>
             <td><?php echo $sale->total_payable; ?></td>
             <td><a href="javascript: return 0;" onclick="search_by_receipt_no('<?php echo $sale->sale_id ?>')">Receipt</a></td>
         </tr>
