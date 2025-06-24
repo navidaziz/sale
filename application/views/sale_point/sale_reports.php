144a5ee47617c3bb6be00e9badda687c549d7094
Navid Aziz <navidaziz98@gmail.com>
2025-06-24 15:18:17 +0500

    some chagnes

--- application/views/sale_point/sale_reports.php
+++ application/views/sale_point/sale_reports.php
@@ -10,36 +10,43 @@
             </div>
             <div class="box-body">
 
-                <table class="table table-bordered">
+                <table class="table table-bordered table-striped table-sm">
                     <thead>
                         <tr>
-                            <th>Type</th>
-                            <th>Items Price</th>
-                            <th>Tax</th>
-                            <th>Discount</th>
-                            <th>Total Sale</th>
-                            <th>Total Profit</th>
+                            <th>Report Metric</th>
+                            <th>Today</th>
+                            <th>This Month</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Today</td>
+                            <td>Items Price</td>
                             <td><?php echo number_format($sale_summary->today_items_price, 2); ?></td>
-                            <td><?php echo number_format($sale_summary->today_tax, 2); ?></td>
-                            <td><?php echo number_format($sale_summary->today_discount, 2); ?></td>
-                            <td><?php echo number_format($sale_summary->today_total_sale, 2); ?></td>
-                            <td><?php echo number_format($profit_summary->today_profit, 2); ?></td>
+                            <td><?php echo number_format($sale_summary->month_items_price, 2); ?></td>
                         </tr>
                         <tr>
-                            <td>This Month</td>
-                            <td><?php echo number_format($sale_summary->month_items_price, 2); ?></td>
+                            <td>Tax</td>
+                            <td><?php echo number_format($sale_summary->today_tax, 2); ?></td>
                             <td><?php echo number_format($sale_summary->month_tax, 2); ?></td>
+                        </tr>
+                        <tr>
+                            <td>Discount</td>
+                            <td><?php echo number_format($sale_summary->today_discount, 2); ?></td>
                             <td><?php echo number_format($sale_summary->month_discount, 2); ?></td>
+                        </tr>
+                        <tr>
+                            <td>Total Sale</td>
+                            <td><?php echo number_format($sale_summary->today_total_sale, 2); ?></td>
                             <td><?php echo number_format($sale_summary->month_total_sale, 2); ?></td>
+                        </tr>
+                        <tr>
+                            <td>Total Profit</td>
+                            <td><?php echo number_format($profit_summary->today_profit, 2); ?></td>
                             <td><?php echo number_format($profit_summary->month_profit, 2); ?></td>
                         </tr>
                     </tbody>
                 </table>
+
 
 
             </div>
