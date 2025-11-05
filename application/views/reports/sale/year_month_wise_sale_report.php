f567246c784c6395cb28288b3a54b438527ce5f5
Navid Aziz <navidaziz98@gmail.com>
2025-11-05 21:41:00 +0500

    some changes

--- application/views/reports/sale/year_month_wise_sale_report.php
+++ application/views/reports/sale/year_month_wise_sale_report.php
@@ -223,7 +223,7 @@
               <td><?= number_format($item_paid, 2); ?></td>
               <td><?= number_format($liabilities_paid, 2); ?></td>
               <td><?= number_format($overall_paid, 2); ?></td>
-              <?php $cash_in_hand_total = (($openning_balance + $sale) - $overall_paid);
+              <?php $cash_in_hand_total = ($overall_paid - ($openning_balance + $sale));
               $item_cash = $cash_in_hand_total - $net_income;
               ?>
               <td><?= number_format($cash_in_hand_total, 2); ?></td>
